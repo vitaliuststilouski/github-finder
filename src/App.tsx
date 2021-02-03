d2589dd621404aef811d31b938d91976893b751d
v.ustilovsky <steelxx@mail.ru>
2021-02-03 21:11:53 +0300

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import { Navbar } from "./components/Navigator/Navbar";
 import { UserList } from "./components/UserList/UserList";
+import { Spinner } from "./components/Spinner/Spinner";
+
 import './App.css';
 
-class App extends Component {
 
+class App extends Component {
+  state = {
+    loading: false,
+    users: [],
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const response = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: response.data, loading: false })
+  }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Navbar navTitle="GitHub" icon="fab fa-github"/>
-          <UserList />
-        </header>
-      </div>
-    );
+
+    if (this.state.loading) {
+      return <Spinner />
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Navbar navTitle="GitHub" icon="fab fa-github" />
+            {/* @ts-ignore */}
+            <UserList loading={this.state.loading} users={this.state.users} />
+          </header>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -3,33 +3,21 @@ import {UserItem} from "../UserItem/UserItem";
 
 import styles from "./userList.module.scss";
 
-export class UserList extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "crocoBot",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "2",
-        login: "crocoBot",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        id: "3",
-        login: "crocoBot",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-    ]
-  }
+interface userItem {
+  id: string; // why not number?
+  login: string;
+  html_url: string;
+  avatar_url: string;
+}
 
+interface userProps {
+  users: Array<userItem>;
+}
+
+export class UserList extends Component<userProps> {
   render() {
-    const userList = this.state.users.map(({id, login, html_url, avatar_url}) => {
-        return <UserItem id={id} login={login} avatarUrl={avatar_url} htmlUrl={html_url}/>
+    const userList = this.props.users.map(({id, login, html_url, avatar_url}) => {
+        return <UserItem key={id} id={id} login={login} avatarUrl={avatar_url} htmlUrl={html_url}/>
       }
     );
 
